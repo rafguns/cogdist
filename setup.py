f6ef0c0734ad682be13f97d1e6dfe464fc9e1042
Raf Guns <raf.guns@uantwerpen.be>
2020-01-07 10:11:06 +0100

    Modernize setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,50 @@
-# coding: utf-8
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
 
 long_description = open('README.md').read()
 
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--cov=cogdist']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
 setup(
-    name='wosfile',
-    version='0.4.1',
-    url='http://github.com/rafguns/wosfile',
+    name='cogdist',
+    version='0.0.1',
     license='New BSD License',
     author='Raf Guns',
-    tests_require=['nose'],
+    tests_require=['pytest', 'pytest-cov'],
+    install_requires=[
+        'numpy>=1.17',
+        'pandas>=0.25',
+    ],
+    python_requires='>=3.6',
+    cmdclass={'test': PyTest},
     author_email='raf.guns@uantwerpen.be',
-    description='Handle Thomson Reuters Web of Science™ export files',
+    description='Measure cognitive distance between publication portfolios',
     long_description=long_description,
-    packages=['wosfile'],
+    packages=['cogdist'],
     platforms='any',
+    test_suite='tests',
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
         'License :: OSI Approved',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development',
         'Topic :: Scientific/Engineering',
         'Development Status :: 3 - Alpha',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-    ]
+    ],
+    extras_require={
+        'testing': ['pytest'],
+    }
 )
