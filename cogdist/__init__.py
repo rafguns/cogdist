7ae72ad0ce64007c76a0e18af059077654c7abcf
Raf Guns <raf.guns@uantwerpen.be>
2022-02-18 20:19:09 +0100

    Change build system to flit

--- cogdist/__init__.py
+++ cogdist/__init__.py
@@ -1,5 +1,9 @@
+"""Measure cognitive distance between publication portfolios"""
+
 from .bootstrap import *
 from .comparison import *
 from .confidenceregion import *
 from .overlap import *
 from .plot import *
+
+__version__ = "0.1"
